5cbf59ff0dac5b4d2bf2c7003ec3412dd0e40eba
Ciprian Caba <caba.ciprian@hoyoslabs.com>
2015-08-01 13:01:29 +0300

    Added turnstile example states and transitions

---
+++ Example/SwiftFSM/TurnstileState.swift
@@ -0,0 +1,27 @@
+//
+//  TurnstileState.swift
+//  SwiftFSM
+//
+//  Created by Ciprian Caba on 01/08/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+enum TurnstileState: String {
+  case Locked = "Locked"
+  case Unlocked = "Unlocked"
+}
+
+extension TurnstileState: Printable {
+  var description: String {
+    get {
+      switch self {
+      case Locked:
+        return "Locked"
+      case Unlocked:
+        return "Unlocked"
+      }
+    }
+  }
+}
---
+++ Example/SwiftFSM/TurnstileTransition.swift
@@ -0,0 +1,27 @@
+//
+//  TurnstileTransition.swift
+//  SwiftFSM
+//
+//  Created by Ciprian Caba on 01/08/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+enum TurnstileTransition: String {
+  case Push = "Push"
+  case Coin = "Coin"
+}
+
+extension TurnstileTransition: Printable {
+  var description: String {
+    get {
+      switch self {
+      case Push:
+        return "Push"
+      case Coin:
+        return "Coin"
+      }
+    }
+  }
+}
