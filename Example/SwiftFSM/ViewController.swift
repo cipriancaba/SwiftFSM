e151ae4e1d1ada2f0143bcc7419f6e209a230d8a
Ciprian Caba <caba.ciprian@hoyoslabs.com>
2015-08-01 13:02:14 +0300

    Initial example implementation

--- Example/SwiftFSM/ViewController.swift
+++ Example/SwiftFSM/ViewController.swift
@@ -7,18 +7,65 @@
 //
 
 import UIKit
+import SwiftFSM
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+  private let fsm = SwiftFSM<TurnstileState, TurnstileTransition>(id: "TurnstileFSM", willLog: false)
+  
+  @IBOutlet var stateTextView: UITextView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    stateTextView.editable = false
+    
+    // add new states and get a reference to them so you can map handlers and define transitions
+    let locked = fsm.addState(.Locked)
+    
+    // Define an inline onEnter handler
+    locked.onEnter = { (transition: TurnstileTransition) -> Void in
+      self.append("Entered the \(self.fsm.currentState) state with the \(transition) transition")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    // Define as many transitions to any states (including itself)
+    locked.addTransition(.Push, to: .Locked)
+    locked.addTransition(.Coin, to: .Unlocked)
+    
+    
+    let unlocked = fsm.addState(.Unlocked)
+    
+    // Define handlers as reference
+    unlocked.onEnter = handleUnlocked
+    // Listen to onExit event
+    unlocked.onExit = handleUnlockedExit
+    
+    unlocked.addTransition(.Coin, to: .Unlocked)
+    unlocked.addTransition(.Push, to: .Locked)
+    
+    // Start the fsm in a specific state.. Please note that this will not trigger the initial onEnter callback
+    fsm.startFrom(.Locked)
+  }
+  
+  func handleUnlocked(transition: TurnstileTransition) {
+    append("Entered the \(self.fsm.currentState) state with the \(transition) transition")
+  }
+  
+  func handleUnlockedExit(transition: TurnstileTransition) {
+    append("Turnstile fsm exited the Unlocked state with the \(transition) transition")
+  }
+  
+  func append(message: String) {
+    stateTextView.text = "\(self.stateTextView.text)\n\(message)"
+    stateTextView.scrollRangeToVisible(NSMakeRange(count(stateTextView.text), 1))
+  }
+  
+  @IBAction func onCoin(sender: AnyObject) {
+    fsm.transitionWith(.Coin)
+  }
+  
+  @IBAction func onPush(sender: AnyObject) {
+    fsm.transitionWith(.Push)
+  }
 }
 
